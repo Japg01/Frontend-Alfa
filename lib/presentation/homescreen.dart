839e7c27c2d49275f2f82fdbbb56430a5d095b33
japg01 <japerezg.2001@gmail.com>
2024-05-03 01:40:58 -0400

    Implementación de los Widgets en el HomeScreen

--- lib/presentation/homescreen.dart
+++ lib/presentation/homescreen.dart
@@ -9,79 +9,74 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        MiScaffold(
-          body: Scaffold(
-            key: _scaffoldKey,
-            appBar: PreferredSize(
-              preferredSize: const Size.fromHeight(230),
-              child: Container(
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [Color(0xFF4F14A0), Color(0xFF8066FF)],
-                  ),
-                ),
-                child: AppBar(
-                  automaticallyImplyLeading: false,
-                  title: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      const Text('Nombre de usuario',
-                          style: TextStyle(fontSize: 28, color: Colors.white)),
-                      const SizedBox(height: 10),
-                      const Text('ID de usuario',
-                          style: TextStyle(fontSize: 15, color: Colors.white)),
-                      const SizedBox(height: 90),
-                      Container(
-                        width: 328,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: const TextField(
-                          decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.search),
-                            labelText: 'Buscar',
-                            border: InputBorder.none,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  backgroundColor: Colors.transparent,
-                  elevation: 0,
-                  actions: <Widget>[
-                    IconButton(
-                      icon: const Icon(Icons.menu, color: Colors.white),
-                      onPressed: () {
-                        if (_scaffoldKey.currentState?.isEndDrawerOpen ??
-                            false) {
-                          _scaffoldKey.currentState?.openEndDrawer();
-                        } else {
-                          _scaffoldKey.currentState?.openEndDrawer();
-                        }
-                      },
-                    ),
-                  ],
-                ),
-              ),
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(230),
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Color(0xFF4F14A0), Color(0xFF8066FF)],
             ),
-            body: ListView(
+          ),
+          child: AppBar(
+            automaticallyImplyLeading: false,
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                buildSection('Fit your body', 328, 104),
-                buildSection('Category of Yoga', 394.14, 105),
-                buildSection('Popular Process', 418, 155),
-                buildSection('Video courses', 415, 172),
-                buildSection('Our Latest blogs', 331, 241),
+                const Text('Nombre de usuario',
+                    style: TextStyle(fontSize: 28, color: Colors.white)),
+                const SizedBox(height: 10),
+                const Text('ID de usuario',
+                    style: TextStyle(fontSize: 15, color: Colors.white)),
+                const SizedBox(height: 90),
+                Container(
+                  width: 328,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.search),
+                      labelText: 'Buscar',
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
               ],
             ),
-            endDrawer: const SideBarMenu(),
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            actions: <Widget>[
+              IconButton(
+                icon: const Icon(Icons.menu, color: Colors.white),
+                onPressed: () {
+                  if (_scaffoldKey.currentState?.isDrawerOpen ?? false) {
+                    _scaffoldKey.currentState?.openDrawer();
+                  } else {
+                    _scaffoldKey.currentState?.openDrawer();
+                  }
+                },
+              ),
+            ],
           ),
         ),
-      ],
+      ),
+      body: MiScaffold(
+        body: ListView(
+          children: <Widget>[
+            buildSection('Fit your body', 328, 104),
+            buildSection('Category of Yoga', 394.14, 105),
+            buildSection('Popular Process', 418, 155),
+            buildSection('Video courses', 415, 172),
+            buildSection('Our Latest blogs', 331, 241),
+          ],
+        ),
+      ),
+      drawer: const SideBarMenu(),
     );
   }
 }
@@ -93,10 +88,25 @@ Widget buildSection(String title, double width, double height) {
     color: Colors.white,
     child: Column(
       children: <Widget>[
-        Container(
-          alignment: Alignment.centerLeft,
-          padding: const EdgeInsets.only(left: 10),
-          child: Text(title, style: const TextStyle(fontSize: 20)),
+        Padding(
+          padding: const EdgeInsets.only(left: 10, top: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                title,
+                style:
+                    const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Ver más',
+                  style: TextStyle(color: Colors.blue),
+                ),
+              ),
+            ],
+          ),
         ),
         Expanded(
           child: ListView.builder(
@@ -106,7 +116,12 @@ Widget buildSection(String title, double width, double height) {
               return Container(
                 width: width,
                 color: Colors.white,
-                child: Center(child: Text(title)),
+                child: Card(
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: Text('Item $index'),
+                  ),
+                ),
               );
             },
           ),
